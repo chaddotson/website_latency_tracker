a8024243b35b46fa10a120f1f36b7aa2cb4e929a
Chad Dotson <chad@cdotson.com>
2017-05-19 11:16:07 -0500

    Switch monitor from endless while loop to independent runs more consistent with a cron job.

--- scripts/monitor_site_responses.py
+++ scripts/monitor_site_responses.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.6
 
-
 from argparse import ArgumentParser
 from csv import writer
 from logging import basicConfig, getLogger, INFO, DEBUG
 import grequests
-from time import sleep, time
+from os.path import exists
+from time import time
 
 logger = getLogger(__name__)
 
@@ -14,8 +14,6 @@ def parse_args():
     parser = ArgumentParser(description='Monitor websites for response times to load the headers.')
     parser.add_argument('tracking_file', help='File to save results to')
     parser.add_argument('urls', help='url list', nargs="+")
-    parser.add_argument('-n', '--new', help='Start new file / erase if present.', default=False, action='store_true')
-    parser.add_argument('-d', '--delay', help='seconds between tests', type=int, default=300)
     parser.add_argument('-v', '--verbose', help='Verbose logs', default=False, action='store_true')
 
     return parser.parse_args()
@@ -32,27 +30,29 @@ if __name__ == "__main__":
     if args.verbose:
         getLogger('').setLevel(DEBUG)
 
-    mode = "w" if args.new else "a"
+    add_header = False
 
-    try:
-        while True:
-            logger.debug("Testing")
+    if not exists(args.tracking_file):
+        logger.debug("File is new, add headers.")
+        add_header = True
 
-            req = (grequests.get(url) for url in args.urls)
-            start = time()
-            resps = grequests.map(req)
+    with open(args.tracking_file, "a") as out:
+        output = writer(out)
 
-            with open(args.tracking_file, mode) as out:
-                output = writer(out)
+        if add_header:
+            output.writerow(["timestamp", "url", "loadtime", "size"])
 
-                for resp in resps:
-                    logger.info("Tested: %s - Response Time: %d - Size: %d",
-                                 resp.url, resp.elapsed.total_seconds(), len(resp.content))
-                    output.writerow([time(), resp.url, resp.elapsed.total_seconds(), len(resp.content)])
+        logger.info("Starting tests")
 
-            sleep(args.delay)
-            mode = "a"
+        req = (grequests.get(url) for url in args.urls)
 
-    except KeyboardInterrupt:
-        logger.info("Exiting...")
+        resps = grequests.map(req)
+
+        for resp in resps:
+            if resp is not None:
+                logger.info("Tested: %s - Response Time: %d - Size: %d",
+                            resp.url, resp.elapsed.total_seconds(), len(resp.content))
+                output.writerow([time(), resp.url, resp.elapsed.total_seconds(), len(resp.content)])
+
+    logger.info("Done")
 
